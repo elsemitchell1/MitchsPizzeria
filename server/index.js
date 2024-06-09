24ec061a8e6a5004110687dac82396f6091d36ae
Mitchell Else <elsemitchell1@gmail.com>
2024-06-09 11:07:17 -0600

    stripe key log

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
     res.sendFile(path);
 });
 app.get("/config", (req, res) => {
+    console.log(process.env.STRIPE_PUBLISHABLE_KEY);
     res.send({
         publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     });
