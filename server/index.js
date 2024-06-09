bfb98604ccc81daf046f66eb4454f1989db364a7
Mitchell Else <elsemitchell1@gmail.com>
2024-06-09 11:23:33 -0600

    env testing 1

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     res.sendFile(path);
 });
 app.get("/config", (req, res) => {
-    console.log(process.env.STRIPE_PUBLISHABLE_KEY);
+    console.log(env.STRIPE_PUBLISHABLE_KEY);
     res.send({
         publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     });
