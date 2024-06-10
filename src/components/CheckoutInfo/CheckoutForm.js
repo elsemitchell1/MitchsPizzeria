f228e6b7d316682f0cc3b508214a971370ee31b4
Mitchell Else <elsemitchell1@gmail.com>
2024-06-10 09:30:32 -0600

    test

--- src/components/CheckoutInfo/CheckoutForm.js
+++ src/components/CheckoutInfo/CheckoutForm.js
@@ -41,7 +41,7 @@ function CheckoutForm({ cartItems }) {
             const { error, paymentIntent } = await stripe.confirmPayment({
                 elements,
                 confirmParams: {
-                    return_url: `${window.location.origin}MitchsPizzeria/#/PaymentSuccess`,
+                    return_url: `${window.location.origin}/MitchsPizzeria/#/PaymentSuccess`,
                 },
             });
 
